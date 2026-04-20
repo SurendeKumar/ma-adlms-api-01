d8fb61a35bfa5941bc39538c15aeeb2a43a9357a
Surender Kumar <45120705+SurendeKumar@users.noreply.github.com>
2026-04-20 14:14:30 +0100

    Temporarily disabled while stabilising CI/CD pipeline test environment

--- libraryapi/src/test/java/com/adlms/libraryapi/LibraryapiApplicationTests.java
+++ libraryapi/src/test/java/com/adlms/libraryapi/LibraryapiApplicationTests.java
@@ -1,15 +1,37 @@
+//package com.adlms.libraryapi;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.ActiveProfiles;
+//
+//@SpringBootTest
+//@ActiveProfiles("test")
+//class LibraryapiApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
+
 package com.adlms.libraryapi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@SpringBootTest(
+    properties = {
+        "spring.cloud.config.enabled=false",
+        "eureka.client.enabled=false",
+        "spring.config.import=",
+        "user.activity.service.base-url=http://localhost:8081"
+    }
+)
 @ActiveProfiles("test")
 class LibraryapiApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
