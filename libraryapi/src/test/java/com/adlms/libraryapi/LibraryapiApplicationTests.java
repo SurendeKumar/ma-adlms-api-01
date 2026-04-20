f475eaa63666a4bd58626b5a8022426a39837045
Surender Kumar <45120705+SurendeKumar@users.noreply.github.com>
2026-04-20 12:28:20 +0100

    updated the application test properties file

--- libraryapi/src/test/java/com/adlms/libraryapi/LibraryapiApplicationTests.java
+++ libraryapi/src/test/java/com/adlms/libraryapi/LibraryapiApplicationTests.java
@@ -2,8 +2,10 @@ package com.adlms.libraryapi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class LibraryapiApplicationTests {
 
 	@Test
