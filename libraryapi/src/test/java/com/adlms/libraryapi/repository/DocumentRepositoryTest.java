dda3c71c40bb9f585cc5870975e4842e8c2171f7
Surender Kumar <45120705+SurendeKumar@users.noreply.github.com>
2026-04-20 12:53:22 +0100

    Updated document repository test to jpadata test

--- libraryapi/src/test/java/com/adlms/libraryapi/repository/DocumentRepositoryTest.java
+++ libraryapi/src/test/java/com/adlms/libraryapi/repository/DocumentRepositoryTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.time.LocalDate;
 import java.util.Optional;
@@ -13,6 +14,7 @@ import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
+@ActiveProfiles("test")
 class DocumentRepositoryTest {
 
     @Autowired
